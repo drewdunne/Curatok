3a5be8880b89095937473c0ba344795a3ece1b4c
drewdunne <57418947+drewdunne@users.noreply.github.com>
2022-09-26 23:23:11 -0700

    [B] <Refactor> Improvments to URL storage methods for scraper

--- src/scraper-test/GetLikedVideos.js
+++ src/scraper-test/GetLikedVideos.js
@@ -3,61 +3,95 @@ const axios = require('axios');
 
 let browser;
 let page;
-const likedVideos = [];
-let likedVideosTargetLength;
+const likedVideos = {};
+let totalLikedVideos;
+let userTitle;
 
 setup('https://www.tiktok.com/@drewdunnehere?lang=en');
 
 async function setup(url) {
     browser = await puppeteer.launch({
-      // headless: false
+      headless: false
         // args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     page = await browser.newPage();
+    await page.goto(url);
+    await getUserTitle();
+    await getProfileLikedVideosCount();
+    console.log(`User ${userTitle} has liked ${totalLikedVideos} videos`);
+    console.log('Collecting videos...');
     await navigateToLikePage(url);
 }
+
+async function getUserTitle() {
+  const userTitleSelector = '[data-e2e="user-title"]';
+  await page.waitForSelector(userTitleSelector);
+  userTitle = await page.$eval(userTitleSelector, el => el.innerText);
+}
+
+async function getProfileLikedVideosCount() {
+  const likeCountSelector = '[data-e2e="likes-count"]'
+  await page.waitForSelector(likeCountSelector);
+  totalLikedVideos = await page.$eval(likeCountSelector, el => el.innerText)
+  console.log(``)
+}
+
 
 // document. querySelector('[data-id="box1"]')
 // data-e2e="liked-tab"
 async function navigateToLikePage(url) {
-    await page.goto(url);
     const likeButton = '[data-e2e*="liked-tab"]';
     await page.waitForSelector('[data-e2e*="liked-tab"]');
-    console.log("found liked-tab")
     const innerText = await page.$eval(likeButton, el => el.innerText);
+    console.log("Navigating to Liked tab via button: " + innerText);
     activateReqInterceptor();
     page.click(likeButton);
-    console.log("Clicked " + innerText);
+    await autoScroll(page);
+    console.log(likedVideos);
+    browser.close();
 }
 
-function activateReqInterceptor() {
+async function activateReqInterceptor() {
     console.log("Interceptor Activated");
-
+    await page.setRequestInterception(true);
     page.on('request', async (interceptedRequest) => {
-      // console.log(interceptedRequest.url());
       const url = interceptedRequest.url();
       if (url.includes('https://us.tiktok.com/api/favorite/'))
       {
         console.log("Intercepted a Like list...")
         await addVideosToVideoUrls(interceptedRequest)
-        console.log('\x1b[32m%s\x1b[0m', `RESOLVED, likedVideos length is now: ${likedVideos.length} / <TOTAL GOES HERE>`);
+        console.log('\x1b[32m%s\x1b[0m', `RESOLVED, likedVideos length is now: ${Object.values(likedVideos).length} / ${totalLikedVideos}`);
       }
-    });
-  }
+      interceptedRequest.continue();
+    })
+    };
 
   async function addVideosToVideoUrls(interceptedRequest)
   {
-    const videoUrls = [];
     const res = await axios.get(interceptedRequest.url())
     const itemList = res.data.itemList;
     itemList.forEach(element => {
+      const urlKey = element.video.bitrateInfo[0].PlayAddr.urlKey;
+      const fileHash = element.video.bitrateInfo[0].PlayAddr.FileHash;
       const url = element.video.bitrateInfo[0].PlayAddr.UrlList[0];
-      videoUrls.push(url);
+      if (!likedVideos[fileHash])
+      {
+        likedVideos[fileHash] = { urlKey: urlKey, url: url }
+      }
     })
-    likedVideos.push(...videoUrls);
   }
 
-    // async function autoScroll(page) {
+  const autoScroll = async (page) => {
+    while (true)
+    {
+      previousHeight = await page.evaluate('document.body.scrollHeight');
+      await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+      await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`)
+      await new Promise((resolve => setTimeout(resolve, 500)))
+    }
+  }
+
+  // async function autoScroll(page) {
     //   await page.evaluate(async () => {
     //     await new Promise((resolve) => {
     //       let totalHeight = 0;
@@ -75,11 +109,6 @@ function activateReqInterceptor() {
     //     });
     //   });
     // }
-
-    // browser.close();
-    // console.log("program terminated");
-
-
 
 
 
